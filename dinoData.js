277d1b15e90fd025d184af14b82afb198a483032
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-01 16:18:36 -0800

    dino-data arrays created with descriptions

---
+++ dinoData.js
@@ -0,0 +1,70 @@
+const data = {
+
+    headOptions: [
+
+        {
+            id: 'tRexHead',
+            description: 'angry',
+            img: 'tRexHeadImg',
+        },
+
+        {
+            id: 'triceratopsHead',
+            description: 'peaceful',
+            img: 'triceratopsHeadImg',
+        },
+
+        {
+            id: 'pterodactylHead',
+            description: 'whimsical',
+            img: 'pterodactylHeadImg',
+        }
+    ],
+
+    torsoOptions: [
+
+        {
+            id: 'tRexTorso',
+            description: 'a carnivore',
+            img: 'tRexTorsoImg',
+        },
+
+        {
+            id: 'triceratopsTorso',
+            description: 'an herbivore',
+            img: 'triceratopsTorsoImg',
+        },
+
+        {
+            id: 'pterodactylTorso',
+            description: 'a scavenger',
+            img: 'pterodactylTorsoImg',
+        }
+    ],
+
+
+    legsOptions: [
+
+        {
+            id: 'tRexLegs',
+            description: '35 mph',
+            img: 'tRexLegsImg',
+        },
+
+        {
+            id: 'triceratopsLegs',
+            description: '12 mph',
+            img: 'triceratopsLegsImg',
+        },
+
+        {
+            id: 'pterodactylLegs',
+            description: '48 mph',
+            img: 'pterodactylLegsImg',
+        }
+    ]
+};
+
+
+export default data;
+
