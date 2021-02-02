1caa467c36931150a9eb22300586cb99036c2037
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-01 16:33:34 -0800

    test developed for grabSum function

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,16 +1,37 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+
+import { grabSum } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+const userTest = {
+
+    userName: 'Alan',
+    dinoArray: [
+        {
+            dinoId: 1,
+            species: 'Raptor',
+            tRexPercent: 70,
+            triceratopsPercent: 20,
+            pterodactylPercent: 10,
+            name: 'Betty',
+            description: 'This is an angry dinosaur.',
+            img: 'headingImg.jpg',
+            top: 35,
+            left: 40,
+        }
+    ]
+};
+
+test('this fuction should take 3 numbers in and return their sum', (expect) => {
     //Arrange
+
+
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 100;
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = grabSum(userTest);
 
     //Expect
     // Make assertions about what is expected versus the actual result
