132cbfe76f76f3ec895ec190449bd92622051ea6
KB <katrinkajb@gmail.com>
2021-02-02 12:00:55 -0800

    wrote renderPosition and getRandomCoordinates functions

---
+++ map/map-utils.js
@@ -0,0 +1,16 @@
+export function renderPosition(user, ul) {
+    const newestDino = user.dinoArray[user.dinoArray.length - 1];
+
+    const dinoTop = newestDino.top;
+    const dinoLeft = newestDino.left;
+
+    const dot = document.createElement('img');
+    dot.src = '../assets/black-dot.jpg';
+    dot.style.width = '30px';
+    dot.style.height = '30px';
+    dot.style.top = `${dinoTop}%`;
+    dot.style.left = `${dinoLeft}%`;
+    dot.style.position = 'absolute';
+
+    ul.append(dot);
+}
--- test/map-utils.test.js
+++ test/map-utils.test.js
@@ -1,18 +1,37 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// import {  } from '../map-utils.js';
 
-const test = QUnit.test;
+// const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+// const userTest = {
+
+//     userName: 'Alan',
+//     dinoArray: [
+//         {
+//             dinoId: 1,
+//             species: 'Raptor',
+//             tRexPercent: 70,
+//             triceratopsPercent: 20,
+//             pterodactylPercent: 10,
+//             name: 'Betty',
+//             description: 'This is an angry dinosaur.',
+//             img: 'headingImg.jpg',
+//             top: 35,
+//             left: 40,
+//         }
+//     ]
+// };
+
+// test('renderPosition should take in', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = true;
     
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = renderPosition(userTest);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
-});
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
