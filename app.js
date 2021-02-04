86e1243ea238920389609de6153f44e380740bba
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-04 11:00:33 -0800

    homepage user input linked to localStorage for userName

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-//const goToLabButton = document.getElementById('lab-button');
+import { getUser, setUser } from './local-storage-utils.js';
+
+const user = getUser();
+const userName = document.getElementById('visitor-sign-in');
+
+
+
+
+
+
 const securityScan = document.getElementById('security-scanner');
 
 let i = 0;
@@ -16,5 +25,45 @@ export function securityInstructions() {
 securityInstructions();
 
 securityScan.addEventListener('click', () => {
+
+    if (!user) {
+        const newUser = {
+            userName: userName.value,
+            dinoArray: [
+                {
+                    dinoId: 0,
+                    species: '',
+                    tRexPercent: '',
+                    triceratopsPercent: '',
+                    pterodactylPercent: '',
+                    name: '',
+                    description: '',
+                    img: '',
+                    top: '',
+                    left: '',
+                }
+            ]
+        };
+        setUser(newUser);
+    }
+    if (user) {
+        // const newDino = {
+        //     dinoId: user.dinoArray[user.dinoArray.length - 1].dinoId + 1,
+        //     species: '',
+        //     tRexPercent: Number(dinoRangeTRex.value),
+        //     triceratopsPercent: Number(dinoRangeTriceratops.value),
+        //     pterodactylPercent: Number(dinoRangePterodactyl.value),
+        //     name: dinoName.value,
+        //     description: '',
+        //     img: '',
+        //     top: coordinateTop,
+        //     left: coordinateLeft,
+        // };
+        //user.dinoArray.push(newDino);
+        setUser(user);
+    }
+
     document.location.href = './lab/';
+
 });
+
