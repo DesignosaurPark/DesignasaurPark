3bcf1035fba235d6d8c1595473adcd095c293a5f
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-04 11:43:44 -0800

    easter-egg functions

---
+++ jeff/jeff-app.js
@@ -0,0 +1,10 @@
+import { chaosAudit } from './jeff-utils.js';
+
+const backAwayButton = document.getElementById('back-to-park');
+
+backAwayButton.addEventListener('click', () => {
+
+    window.location = '../lab/index.html';
+
+});
+
