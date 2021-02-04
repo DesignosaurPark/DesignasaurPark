c3b63fe47af284aab1870a6d94e9277ec1c41df3
SamuelHGerber <henrylightfoot@gmail.com>
2021-02-04 10:51:44 -0800

    security instructions type-out added

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 //const goToLabButton = document.getElementById('lab-button');
 const securityScan = document.getElementById('security-scanner');
 
+let i = 0;
+let text = 'Please enter your name and then scan your thumbprint for security clearance:';
+const speed = 50;
+
+export function securityInstructions() {
+    if (i < text.length) {
+        document.getElementById('visitor-instructions').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(securityInstructions, speed);
+    }
+}
+
+securityInstructions();
+
 securityScan.addEventListener('click', () => {
     document.location.href = './lab/';
 });
