5ad0f92ee98f6a64d2c4cb6c32d2046a07d6c1dd
KB <katrinkajb@gmail.com>
2021-02-05 10:54:35 -0800

    deleting commented out code

--- incubation/incubation-utils.js
+++ incubation/incubation-utils.js
@@ -21,7 +21,6 @@ export function renderDinosaur(dinoPartIdArray) {
         dinoImg.style.display = 'flex';
         dinoImg.style.flexDirection = 'column';
         dinoImg.style.placeContent = 'center';
-        // dinoImg.style.justifyContent = 'center';
         dinoImg.style.width = '264px';
         dinoImg.style.marginLeft = '40vw';
         dinoImg.style.marginRight = '40vw';
